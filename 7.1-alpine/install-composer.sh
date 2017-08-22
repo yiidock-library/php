b0ad593291ef055de5b122c448e203506e2915de
Alexey Samoylov <alexey.samoylov@gmail.com>
2017-08-22 17:37:01 +0700

    fixed composer installer

--- 7.1-alpine/install-composer.sh
+++ 7.1-alpine/install-composer.sh
@@ -1,17 +1,2 @@
 #!/bin/sh
-
-EXPECTED_SIGNATURE=$(curl https://composer.github.io/installer.sig)
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
-
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
-fi
-
-php composer-setup.php --install-dir=/usr/bin --filename=composer
-RESULT=$?
-rm composer-setup.php
-exit $RESULT
+wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php -- --quiet --install-dir=/usr/local/bin --filename=composer
