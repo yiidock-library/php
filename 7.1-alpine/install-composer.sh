5ea4f6d657ce9e9003ee3893166b34f048458610
Alexey Samoylov <alexey.samoylov@gmail.com>
2017-08-23 12:36:28 +0700

    added composer installer to the dockerfile

--- 7.1-alpine/install-composer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php -- --quiet --install-dir=/usr/local/bin --filename=composer
